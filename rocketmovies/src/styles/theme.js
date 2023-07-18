764cbfb89979e18ea28f8b467137128e8bc178e4
Vinicius <vini1718a@gmail.com>
2023-07-18 20:32:26 -0300

    adding default colors and themes

---
+++ rocketmovies/src/styles/theme.js
@@ -0,0 +1,11 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: "#1C1B1E",
+    BACKGROUND_800: "#262529",
+    PINK: "#FF859B",
+    DARK_PINK: "#522f34", // mais escuro
+
+    GRAY_300: "#312E38",
+    WHITE: "#F4EDE8",
+  },
+};
